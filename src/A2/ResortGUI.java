360c8d81c2ed25dec5c8cc2bb89a38141e3b8cfd
TylerLillis <s3947709@student.rmit.edu.au>
2024-10-24 17:24:10 +1100

    Making some GUI layout

--- src/A2/ResortGUI.java
+++ src/A2/ResortGUI.java
@@ -5,15 +5,22 @@ import javax.swing.*;
 class ResortGUI extends JFrame implements EventListener{
 
     private JTabbedPane tabbedPane2;
-    private JButton addCustomerButton;
-    private JButton listCustomersButton;
-    private JButton viewAvailableAccommodationsButton;
-    private JButton viewAllAccommodationsButton1;
-    private JButton createTavelPackageButton;
-    private JButton viewTravelPackageButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton addLiftPassButton;
+    private JButton addLessonsButton;
+    private JButton viewTravelPackagesButton1;
+    private JButton createTravelPackageButton;
+    private JButton readSavedPackagesButton;
     private JButton savePackageButton;
-    private JButton readPackageButton;
-    private JButton addLiftPassToButton;
-    private JButton addLessonsToPackageButton;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JComboBox comboBox1;
+    private JButton listCustomersButton;
+    private JButton addCustomerButton;
+    private JButton viewAllAccommodationsButton;
+    private JButton viewAvailableAccommodationsButton;
 
 }
